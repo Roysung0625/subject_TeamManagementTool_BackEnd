317d2c8ffbd46397dec73f2127497571018676c8
Roysung <roy06255@gmail.com>
2025-05-24 22:26:04 +0900

    fix: 重複url修正

--- config/routes.rb
+++ config/routes.rb
@@ -14,13 +14,13 @@ Rails.application.routes.draw do
     # タスク関連エンドポイント
     resources :tasks, only: %i[create update destroy show] do
       # 特定社員の本日のタスク取得
-      get    ':employee_id/today', to: 'tasks#employee_today', on: :collection
+      get    'list/:employee_id/today', to: 'tasks#employee_today', on: :collection
       # 特定社員の全タスク取得（フィルタリング: ?category=&status=）
-      get    ':employee_id', to: 'tasks#index_by_employee', on: :collection
+      get    'list/:employee_id', to: 'tasks#index_by_employee', on: :collection
       # 特定チームの全タスク取得
-      get    ':team_id', to: 'tasks#index_by_team', on: :collection
+      get    'list/:team_id', to: 'tasks#index_by_team', on: :collection
       # 特定チームの本日のタスク取得
-      get    ':team_id/today', to: 'tasks#team_today', on: :collection
+      get    'list/:team_id/today', to: 'tasks#team_today', on: :collection
     end
 
     # 認証関連エンドポイント
