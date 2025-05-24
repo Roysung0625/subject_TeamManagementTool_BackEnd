df1c2902bb74210382afb4fb4c631bc9483466a4
Roysung <roy06255@gmail.com>
2025-05-25 03:54:44 +0900

    fix: 重複url修正

--- config/routes.rb
+++ config/routes.rb
@@ -7,20 +7,20 @@ Rails.application.routes.draw do
       # チームメンバー管理
       patch  'management/:team_id', to: 'teams#update_members', on: :collection
       # 特定社員のチーム一覧取得
-      get    ':employee_id', to: 'teams#index_by_employee', on: :collection
-      get    ':team_id', to: 'teams#index_by_team', on: :collection
+      get    'employee/:employee_id', to: 'teams#index_by_employee', on: :collection
+      get    'team/:team_id', to: 'teams#index_by_team', on: :collection
     end
 
     # タスク関連エンドポイント
     resources :tasks, only: %i[create update destroy show] do
       # 特定社員の本日のタスク取得
-      get    'list/:employee_id/today', to: 'tasks#employee_today', on: :collection
+      get    'employee/:employee_id/today', to: 'tasks#employee_today', on: :collection
       # 特定社員の全タスク取得（フィルタリング: ?category=&status=）
-      get    'list/:employee_id', to: 'tasks#index_by_employee', on: :collection
+      get    'employee/:employee_id', to: 'tasks#index_by_employee', on: :collection
       # 特定チームの全タスク取得
-      get    'list/:team_id', to: 'tasks#index_by_team', on: :collection
+      get    'team/:team_id', to: 'tasks#index_by_team', on: :collection
       # 特定チームの本日のタスク取得
-      get    'list/:team_id/today', to: 'tasks#team_today', on: :collection
+      get    'team/:team_id/today', to: 'tasks#team_today', on: :collection
     end
 
     # 認証関連エンドポイント
